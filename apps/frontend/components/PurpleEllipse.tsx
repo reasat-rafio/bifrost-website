95882de544c3814ef47ebef8d4af85e898427073
RielJ <bulaybulay.rielj@gmail.com>
2022-02-22 13:14:55 +0800

    Transition Ellipse Update.

--- apps/frontend/components/PurpleEllipse.tsx
+++ apps/frontend/components/PurpleEllipse.tsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion'
-import { useVisibleScrollEffect } from 'lib/hooks'
+import { animationFrameEffect, useVisibleScrollEffect } from 'lib/hooks'
 import { ReactElement, RefObject, useState } from 'react'
 import { useWindowSize } from 'react-use'
 
@@ -9,71 +9,57 @@ interface PurpleEllipseProps {
 }
 
 export default function PurpleEllipse(props: PurpleEllipseProps): ReactElement {
-  const { height: windowHeight, width: windowWidth } = useWindowSize() ?? {
+  const { height: windowHeight } = useWindowSize() ?? {
     width: 0,
     height: 0,
   }
   const [ratio, setRatio] = useState(0.3)
+
   useVisibleScrollEffect(
     props.rootRef,
-    (offsetBoundingRect, _, y) => () => {
-      const yDelta = y + windowHeight - offsetBoundingRect.top
-      const ratio = yDelta / windowHeight
-      console.log({ yDelta, ratio, windowHeight, offsetBoundingRect, y })
-      if (
-        y > offsetBoundingRect.bottom - windowHeight / 2 ||
-        y < offsetBoundingRect.top - windowHeight / 3
-      ) {
-        setRatio(0.3)
-      } else {
-        setRatio(1.3)
-      }
-    },
+    (offsetBoundingRect, _, y) =>
+      animationFrameEffect(() => {
+        const yDelta = y + windowHeight - offsetBoundingRect.top
+        console.log({ yDelta, windowHeight, offsetBoundingRect, y })
+        if (yDelta < windowHeight) {
+          setRatio(yDelta / windowHeight)
+        } else if (yDelta > offsetBoundingRect.height) {
+          setRatio(1 - (yDelta - offsetBoundingRect.height) / windowHeight)
+        } else {
+          setRatio(1)
+        }
+        console.log({ ratio })
+      }),
     [ratio],
   )
   return (
     <div className="absolute top-0 h-[100%]">
-      <div className="sticky top-0 w-[90vw] h-[100vh]">
+      <div className="sticky top-0 w-[100vw] h-[100vh]">
         <motion.div
-          className="z-0 absolute rounded-full md:w-[20vw] md:h-[20vw] w-[40vw] h-[40vw] top-[30vh] left-[0vw] bg-gradient-to-r from-[#594CA6] via-[#5F4C9D] to-[#3E2F5E]"
+          className="z-10 absolute rounded-full md:w-[40vw] md:h-[40vw] w-[50vw] h-[50vw] top-[30vh] left-[0vw] bg-gradient-to-r from-[#594CA6] via-[#5F4C9D] to-[#3E2F5E]"
           animate={{
             filter: 'blur(98px)',
             scale: ratio,
-            translateX: (windowWidth / 10) * (ratio - 0.3),
-            translateY: (windowHeight / 10) * (ratio - 0.3),
-            transition: {
-              ease: 'easeInOut',
-              delay: 0.5,
-              duration: 1,
-            },
+            translateX: `${10 * ratio}%`,
+            translateY: (windowHeight / 10) * ratio,
           }}
         />
         <motion.div
-          className="z-10 absolute top-[10vh] rounded-full md:w-[16vw] md:h-[16vw] w-[30vw] h-[30vw] right-[0vw] bg-gradient-to-r from-[#594CA6] via-[#5F4C9D] to-[#3E2F5E]"
+          className="z-10 absolute rounded-full md:w-[36vw] md:h-[36vw] w-[30vw] h-[30vw] right-[0vw] bg-gradient-to-r from-[#594CA6] via-[#5F4C9D] to-[#3E2F5E]"
           animate={{
             filter: 'blur(58px)',
             scale: ratio,
-            translateX: -(windowWidth / 30) * (ratio - 0.3),
-            translateY: (windowHeight / 10) * (ratio - 0.3),
-            transition: {
-              ease: 'easeIn',
-              delay: 0.5,
-              duration: 1,
-            },
+            translateX: `-${20 * ratio}%`,
+            translateY: (windowHeight / 10) * ratio,
           }}
         />
         <motion.div
-          className="z-10 absolute top-[80vh]  rounded-full md:w-[16vw] md:h-[16vw] w-[30vw] h-[30vw] right-[0vw] bg-gradient-to-r from-[#594CA6] via-[#5F4C9D] to-[#3E2F5E]"
+          className="z-10 absolute top-[80vh] rounded-full md:w-[26vw] md:h-[26vw] w-[30vw] h-[30vw] right-[0vw] bg-gradient-to-r from-[#594CA6] via-[#5F4C9D] to-[#3E2F5E]"
           animate={{
             filter: 'blur(58px)',
             scale: ratio,
-            translateX: -ratio * 100 + 100 * 1.3,
-            translateY: ratio * 150 - 150 * 1.3,
-            transition: {
-              ease: 'easeIn',
-              delay: 0.5,
-              duration: 1,
-            },
+            translateX: `-${70 * ratio}%`,
+            translateY: -(windowHeight / 10) * ratio,
           }}
         />
       </div>
