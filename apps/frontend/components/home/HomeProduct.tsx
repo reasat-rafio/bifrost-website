998f2c36b0a7edc35ef6034b3fa59214e78a3e26
reasat-rafio <rafioar96@gmail.com>
2022-03-11 04:35:24 +0600

    :iphone: product section responsiveness update

--- apps/frontend/components/home/HomeProduct.tsx
+++ apps/frontend/components/home/HomeProduct.tsx
@@ -9,7 +9,7 @@ export default function HomeProduct(data: ProductSection): ReactElement {
     <div className="container z-10 my-20 text-white">
       <div className="grid grid-cols-12 space-y-5 lg:space-y-0 relative lg:space-x-12">
         <div className="lg:col-span-6 col-span-12 lg:-mr-1/16 2xl:-mr-1/8 w-full self-center">
-          <div className=" grid grid-cols-2 md:gap-4 gap-2">
+          <div className="grid grid-cols-2 md:gap-4 gap-2">
             {data.images.map((image) => (
               <SanityImg
                 key={image?.alt}
@@ -17,13 +17,13 @@ export default function HomeProduct(data: ProductSection): ReactElement {
                 image={image}
                 alt={image?.alt}
                 height={250}
-                className="w-full object-cover"
+                className="w-full lg:max-h-[262px] object-cover rounded-[8px]"
               />
             ))}
           </div>
         </div>
-        <div className="flex flex-col lg:col-span-6 col-span-12 space-y-8 bifrost__transparent_card md:py-[5rem] md:px-[3.75rem] py-[3rem] px-[2rem]">
-          <div className="flex flex-col space-y-5">
+        <div className="flex flex-col lg:col-span-6 col-span-12 space-y-5 bifrost__transparent_card md:py-[5rem] md:px-[3.75rem] py-[3rem] px-[2rem]">
+          <div className="flex flex-col space-y-3">
             <h3 className="md:text-body-1 text-[18px] leading-[22.86px] font-[400] bifrost__gradient_green text-transparent bg-clip-text uppercase">
               {data.subHeadline}
             </h3>
@@ -31,7 +31,9 @@ export default function HomeProduct(data: ProductSection): ReactElement {
               {data.headline}
             </h1>
           </div>
-          <div className="md:text-body-1 text-[14px] leading-[20px] opacity-[0.7]">{data.body}</div>
+          <div className="md:text-body-1 pt-1 text-[14px] leading-[20px] opacity-[0.7]">
+            {data.body}
+          </div>
           <div className="flex">
             <Button color="secondary">
               <a href={data.ctaButton.href}>{data.ctaButton.title}</a>
