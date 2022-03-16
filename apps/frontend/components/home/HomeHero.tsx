553c185897688a18e8b511fc97496df8903ef922
reasat-rafio <rafioar96@gmail.com>
2022-03-16 08:50:02 +0600

    :wastebasket: pkg removed from root

--- apps/frontend/components/home/HomeHero.tsx
+++ apps/frontend/components/home/HomeHero.tsx
@@ -38,7 +38,7 @@ export default function HomeHero(data: HomeSection): ReactElement {
       </div>
       <div className="container lg:pt-16 pt-24 relative w-screen overflow-y-clip min-h-screen grid grid-cols-12 z-10 ">
         <div className="lg:col-span-7 xl:col-span-8 col-span-12 flex flex-col items-start space-y-10 self-center">
-          <div className="font-roboto lg:text-head-1 text-[82px] leading-[82px]">
+          <div className="font-roboto font-[275] lg:text-head-1 text-[82px] leading-[82px]">
             <h1>{data.headline}</h1>
             <h1 className="text-transparent bg-clip-text bifrost__gradient_green">
               {data.subHeadline}
