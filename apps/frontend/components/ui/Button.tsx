ba8af81581ff9b41f9f13c85cbb86e687a5cd0a6
reasat-rafio <rafioar96@gmail.com>
2022-03-16 16:52:12 +0600

    :lipstick: button ui and animation update

--- apps/frontend/components/ui/Button.tsx
+++ apps/frontend/components/ui/Button.tsx
@@ -2,7 +2,6 @@ import clsx from 'clsx'
 import React, { forwardRef, ButtonHTMLAttributes } from 'react'
 import { motion } from 'framer-motion'
 import styles from '@styles/button.module.scss'
-import { GradientBorder } from 'components/common/GradientBorder'
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string
@@ -27,9 +26,6 @@ const Button = forwardRef<HTMLButtonElement, any>((props, ref) => {
 
   const rootClassName = clsx(
     'outline-none xl:text-base text-sm text-white xl:px-8 px-10 py-3 xl:py-3 2xl:py-3 transition-all ease-in-out duration-300 xl:w-auto w-full relative rounded-[4px]',
-    // variant === 'primary' && styles.primary,
-    // variant === 'secondary' && styles.secondary,
-    styles.button,
     loading && 'cursor-not-allowed',
     disabled && 'cursor-not-allowed hover:cursor-not-allowed brightness-75',
     className,
@@ -38,30 +34,39 @@ const Button = forwardRef<HTMLButtonElement, any>((props, ref) => {
   return (
     <>
       {variant === 'primary' && (
-        <GradientBorder borderRadious="4px">
-          <motion.button
-            ref={ref}
-            aria-pressed={active}
-            disabled={disabled}
-            className={clsx(rootClassName, styles.primary)}
-            {...rest}
-          >
-            {children}
-          </motion.button>
-        </GradientBorder>
+        <motion.button
+          whileHover={{
+            scale: 1.03,
+          }}
+          type="button"
+          aria-pressed={active}
+          ref={ref}
+          className={clsx(rootClassName, styles.primary_border_gradient)}
+          disabled={disabled}
+          {...rest}
+        >
+          {children}
+        </motion.button>
       )}
+
       {variant === 'secondary' && (
-        <motion.div>
-          <motion.button
-            ref={ref}
-            aria-pressed={active}
-            disabled={disabled}
-            className={clsx(rootClassName, styles.secondary)}
-            {...rest}
-          >
-            {children}
-          </motion.button>
-        </motion.div>
+        <motion.button
+          whileHover={{
+            scale: 1.03,
+          }}
+          type="button"
+          aria-pressed={active}
+          ref={ref}
+          className={clsx(
+            rootClassName,
+            styles.secondary_bg_gradient,
+            styles.secondary_border_gradient,
+          )}
+          disabled={disabled}
+          {...rest}
+        >
+          {children}
+        </motion.button>
       )}
     </>
   )
