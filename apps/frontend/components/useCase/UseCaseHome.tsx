593161f8d186be19e40eb3b2b63453b328370202
reasat-rafio <rafioar96@gmail.com>
2022-03-16 07:15:17 +0600

    :iphone: use case hero responsiveness complete

--- apps/frontend/components/useCase/UseCaseHome.tsx
+++ apps/frontend/components/useCase/UseCaseHome.tsx
@@ -1,4 +1,3 @@
-import clsx from 'clsx'
 import ThreeJSWaves from 'components/ThreeJSWaves'
 import Button from 'components/ui/Button'
 import { HomeSection } from 'lib/useCaseTypes'
@@ -14,20 +13,19 @@ export default function UseCaseHome(data: HomeSection): ReactElement {
           <ThreeJSWaves />
         </div>
       </div>
-      <div
-        className={clsx(
-          'container lg:pt-16 pt-24 flex flex-col xl:w-[60%] justify-center items-center text-center z-10 md:space-y-8 space-y-8 relative w-[100vw] min-h-[100vh] overflow-y-clip lg:py-0 py-52',
-          'text-white',
-        )}
-      >
-        <div className="bg-clip-text bifrost__gradient_green text-transparent uppercase lg:text-head-6 text-head-6">
-          {data.subHeadline}
+      <div className="container min-h-screen overflow-y-clip lg:py-[5%] py-[30%] flex flex-col justify-center items-center">
+        <div className="text-center max-w-4xl flex flex-col lg:space-y-12 space-y-8">
+          <h2 className="bg-clip-text bifrost__gradient_green text-transparent uppercase lg:text-head-6 text-head-6">
+            {data.subHeadline}
+          </h2>
+          <h1 className="lg:text-head-1 text-[82px] leading-[82px] font-[275]">{data.headline}</h1>
+          <p className="lg:text-body-2 text-body-2 font-[300] opacity-70">{data.body}</p>
+          <div className="flex mx-auto">
+            <Button variant="secondary">
+              <a href={data.ctaButton.href}>{data.ctaButton.title}</a>
+            </Button>
+          </div>
         </div>
-        <div className="lg:text-head-1 text-[82px] leading-[82px] font-[275]">{data.headline}</div>
-        <div className="lg:text-body-2 text-body-2 font-[300] opacity-70">{data.body}</div>
-        <Button color="secondary">
-          <a href={data.ctaButton.href}>{data.ctaButton.title}</a>
-        </Button>
       </div>
       <div
         className="z-10 absolute bottom-0 left-0 h-[30vh] w-full"
