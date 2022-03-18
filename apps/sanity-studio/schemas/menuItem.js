47db9e1cacd13fcbf5d06da66463d10bbbe202e2
reasat-rafio <rafioar96@gmail.com>
2022-03-18 16:39:11 +0600

    :hammer: navbar twaek

--- apps/sanity-studio/schemas/menuItem.js
+++ apps/sanity-studio/schemas/menuItem.js
@@ -1,25 +1,26 @@
-import { MdLink } from "react-icons/md";
+import { MdLink } from 'react-icons/md'
 
 export default {
-  name: "menuItem",
-  title: "Menu Item",
-  type: "object",
+  name: 'menuItem',
+  title: 'Menu Item',
+  type: 'object',
   icon: MdLink,
   fields: [
-    { name: "title", title: "Title", type: "string" },
-    { name: "href", title: "Link", type: "string" },
-    { name: "isCTA", title: "Is CTA", type: "boolean" },
+    { name: 'title', title: 'Title', type: 'string' },
+    { name: 'href', title: 'Link', type: 'string' },
+    { name: 'url', title: 'URL', type: 'url' },
+    { name: 'isCTA', title: 'Is CTA', type: 'boolean' },
     {
-      name: "submenu",
-      title: "Submenu",
-      type: "array",
-      of: [{ type: "menuItem" }],
+      name: 'submenu',
+      title: 'Submenu',
+      type: 'array',
+      of: [{ type: 'menuItem' }],
     },
   ],
   preview: {
     select: {
-      title: "title",
-      subtitle: "href",
+      title: 'title',
+      subtitle: 'href',
     },
   },
-};
+}
