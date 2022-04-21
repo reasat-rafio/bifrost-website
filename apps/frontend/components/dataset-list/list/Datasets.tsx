e5c6ccb3476ac31d31419eb40b1f8ec1a120ce63
reasat-rafio <rafioar96@gmail.com>
2022-04-21 13:24:51 +0600

    :hammer: tooltip added

--- apps/frontend/components/dataset-list/list/Datasets.tsx
+++ apps/frontend/components/dataset-list/list/Datasets.tsx
@@ -1,12 +1,15 @@
 import clsx from 'clsx'
 import { ICategory, IDatasetCard } from 'lib/@types/datasetTypes'
 import { useWindowSize } from 'lib/hooks'
+import Link from 'next/link'
 import React, { useEffect, useRef, useState } from 'react'
 import { SanityImg } from 'sanity-react-extra'
 import { imageUrlBuilder } from 'utils/sanity'
 import { RelevanceFiltering } from './RelevanceFiltering'
 import { SearchAndFiltering } from './search-filtering/SearchAndFiltering'
-
+import ReactTooltip from 'react-tooltip'
+import Tooltip from 'rc-tooltip'
+import 'rc-tooltip/assets/bootstrap.css'
 interface DatasetsProps {
   className?: string
   datasets: IDatasetCard[]
@@ -29,18 +32,14 @@ const DatasetCards: React.FC<IDatasetCards> = ({
   index,
 }) => {
   const windowWidth = useWindowSize()?.width ?? 0
-
   const tasksRef = useRef<HTMLDivElement | null>(null)
 
   const [taskListNodes, setTaskListNodes] = useState<NodeListOf<HTMLElement> | []>([])
-
   const [tasksContainerWidth, setTasksContainerWidth] = useState(0)
   const [totalTasksWidth, setTotalTasksWidth] = useState(0)
   const [showMoreTasksLabelWidth, setShowMoreTasksLabelWidth] = useState(0)
-
   const [overflownTasks, setOverflownTasks] = useState<null | string[]>([])
-
-  console.log(overflownTasks)
+  const [showToolTip, setShowToopTip] = useState(false)
 
   useEffect(() => {
     setTotalTasksWidth(0)
@@ -98,15 +97,25 @@ const DatasetCards: React.FC<IDatasetCards> = ({
   }, [taskListNodes.length, windowWidth])
 
   return (
-    <div className="col-span-4 background__dark rounded-[15px] ">
+    <div className="xl:col-span-4 col-span-6 background__dark rounded-[15px] h-full group">
       <div className="flex flex-col space-y-1 border-b border-[#1E2531] p-3 font-light ">
-        <SanityImg
-          className="rounded-[14px]"
-          image={image}
-          builder={imageUrlBuilder}
-          alt={`${heading}'s image`}
-        />
-        <h6 className="truncate text-[22px]">{heading}</h6>
+        <div className="h-[215px] rounded-[14px] overflow-hidden mb-1">
+          <SanityImg
+            className="h-full w-full object-cover group-hover:scale-105 transition-all duration-300"
+            image={image}
+            builder={imageUrlBuilder}
+            alt={`${heading}'s image`}
+          />
+        </div>
+
+        <Link href={`/datasets/${slug.current}`}>
+          <a>
+            <h6 className="truncate text-[22px] cursor-pointer  hover:bg-gradient-to-l from-[#eeffe9] via-[#acffeb] to-[#c9ff71] hover:text-transparent duration-300 hover:bg-clip-text text-white font-light transition-none">
+              {heading}
+            </h6>
+          </a>
+        </Link>
+
         <span className="text-[14px]">{subHeading}</span>
       </div>
       <div className="px-3 py-3 flex flex-col space-y-2 text-[14px]">
@@ -121,24 +130,66 @@ const DatasetCards: React.FC<IDatasetCards> = ({
         >
           {tasks?.map(({ name, _id }) => (
             <p
-              aria-hidden={false}
               className={`bifrost__gradient__green text-transparent bg-clip-text datasets-tasks-${index}`}
               key={_id}
             >
               {name}{' '}
             </p>
           ))}
-          <p
+          {/* <p
+            // data-tip
+            // data-for="getContent"
             className={clsx(
               `bifrost__gradient__green text-transparent bg-clip-text cursor-pointer show-more-${index}`,
               overflownTasks.length ? 'visible' : 'invisible',
             )}
           >
             + {overflownTasks.length} More
-          </p>
+          </p> */}
+
+          <Tooltip
+            placement="topLeft"
+            visible={showToolTip}
+            animation="zoom"
+            onVisibleChange={() => setShowToopTip((prev) => !prev)}
+            trigger="click"
+            overlayInnerStyle={{
+              background: '#0D0E14',
+              border: '1px solid #1E2531',
+              opacity: '1',
+            }}
+            overlay={
+              <span className="text-[14px] text-opacity-70 font-light ">
+                {overflownTasks.map((e: string, idx: number) =>
+                  idx === overflownTasks.length - 1 ? e : `${e}, `,
+                )}
+              </span>
+            }
+          >
+            <p
+              className={clsx(
+                `bifrost__gradient__green text-transparent bg-clip-text cursor-pointer show-more-${index}`,
+                overflownTasks.length ? 'visible' : 'invisible',
+              )}
+            >
+              + {overflownTasks.length} More
+            </p>
+          </Tooltip>
+
+          {/* <ReactTooltip
+            className="customeTheme"
+            id="getContent"
+            getContent={[
+              () => {
+                return overflownTasks.map((e) => e)
+              },
+              1000,
+            ]}
+            // getContent={() => overflownTasks.map((e) => e)}
+          /> */}
         </div>
-        <div className="flex space-x-2 text-opacity-70">
-          <span className="flex-1 underline">716 Images</span>
+        <div className="flex space-x-10 text-opacity-70">
+          <span className="underline">716 Images</span>
           <span>YOLO Format</span>
         </div>
       </div>
@@ -155,9 +206,7 @@ export const Datasets: React.FC<DatasetsProps> = ({
   return (
     <div className={className}>
       <SearchAndFiltering labelFormat={labelFormat} taskTypes={taskTypes} />
-
       <RelevanceFiltering length={datasets.length} />
-
       <div className="grid grid-cols-12 gap-7 ">
         {datasets.map((dataset, index) => (
           <DatasetCards key={dataset._id} {...dataset} index={index} />
