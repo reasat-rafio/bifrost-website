3a3194d71bf925d4b51a61b7554c999fb14f3ee3
reasat-rafio <rafioar96@gmail.com>
2022-05-06 05:37:38 +0600

    :hammer: tweak

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -3,9 +3,9 @@ const withPlugins = require('next-compose-plugins')
 const withTM = require('next-transpile-modules')(['three'])
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NX_SANITY_PROJECT_ID,
-    NEXT_PUBLIC_SANITY_DATASET: process.env.NX_SANITY_DATASET,
-    SANITY_API_TOKEN: process.env.NX_SANITY_API_TOKEN,
+    NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.SANITY_PROJECT_ID,
+    NEXT_PUBLIC_SANITY_DATASET: process.env.SANITY_DATASET,
+    SANITY_API_TOKEN: process.env.SANITY_API_TOKEN,
     FULL_PAGE_KEY: process.env.FULL_PAGE_KEY,
     NEXT_PUBLIC_FORM_ID: process.env.NEXT_PUBLIC_FORM_ID,
   },
