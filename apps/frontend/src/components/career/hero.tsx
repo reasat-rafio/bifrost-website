ca9f3397cbc4b9ae87cafe71ec4e37cdfec7bba5
reasat-rafio <rafioar96@gmail.com>
2022-12-11 00:26:38 +0600

    :hammer: hero text poisition update

--- apps/frontend/src/components/career/hero.tsx
+++ apps/frontend/src/components/career/hero.tsx
@@ -23,13 +23,13 @@ export const Hero: React.FC<HeroProps> = ({ headline, subHeadline, setHeroSectio
       ref={sectionRef}
       className="z-0 fixed top-0 left-0 | w-screen | bg-black overflow-hidden"
     >
-      <WaveScene className="xl:translate-y-[55%] md:translate-y-[45%] translate-y-[20%]" />
+      <WaveScene className="md:translate-y-[55%] translate-y-[40%]" />
 
-      <div className="container | xl:h-[60vh] md:h-[60vh] h-screen | flex flex-col justify-center items-center">
-        <h1 className="xl:text-head-1 md:text-head-3 text-head-mobile font-[275] | mb-3 | text-transparent bg-clip-text text-center | leading-none | gradient__white__to__green">
+      <div className="container | xl:h-[60vh] md:h-[60vh] h-[70vh] | flex flex-col justify-center items-center">
+        <h1 className="w-full | xl:text-head-1 md:text-head-3 text-head-mobile font-[275] | mb-3 | text-transparent bg-clip-text md:text-center text-left | leading-none | gradient__white__to__green">
           {headline}
         </h1>
-        <p className="md:text-body-2 text-body-mobile bg-clip-text opacity-70 text-center">
+        <p className="w-full | md:text-body-2 text-body-mobile md:text-center text-left | bg-clip-text opacity-70 font-light">
           {subHeadline}
         </p>
       </div>
--- apps/frontend/src/components/common/information.tsx
+++ apps/frontend/src/components/common/information.tsx
@@ -18,7 +18,7 @@ export const Information: React.FC<InformationProps> = ({ image, subtitle, title
     <section className="container | py-10 ">
       <figure className="w-full overflow-hidden">
         <SanityImg
-          className="w-full h-full max-h-[70vh] | object-cover rounded-2xl"
+          className="w-full h-full max-h-[560px] | object-cover rounded-2xl"
           builder={imageUrlBuilder}
           width={1000}
           image={image}
@@ -27,8 +27,9 @@ export const Information: React.FC<InformationProps> = ({ image, subtitle, title
 
       <section className="flex justify-end">
         <div className="max-w-lg | flex flex-col space-y-3 | p-5 | -translate-y-1/2 mr-[5%] | bifrost__transparent_card">
-          <Header>{title}</Header>
-          <Description>{subtitle}</Description>
+          <h6 className="xl:text-head-4">{title}</h6>
+          {/* <Header>{title}</Header> */}
+          {/* <Description>{subtitle}</Description> */}
           {!!cta && (
             <div className="flex">
               <Button>
