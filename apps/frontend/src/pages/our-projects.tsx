02e0154d967968a1c009910addd4b0a9d18543ff
reasat-rafio <rafioar96@gmail.com>
2022-12-12 00:16:02 +0600

    :construction: project carousel added

--- apps/frontend/src/pages/our-projects.tsx
+++ apps/frontend/src/pages/our-projects.tsx
@@ -1,6 +1,7 @@
 import { Contact } from 'components/common/contact'
 import Newsletter from 'components/newsletter'
 import { Hero, HeroProps } from 'components/our-projects/hero'
+import { Projects } from 'components/our-projects/projects'
 import { siteQuery } from 'lib/query'
 import { GetStaticProps, GetStaticPropsContext } from 'next'
 import { groq } from 'next-sanity'
@@ -46,6 +47,7 @@ const OurProjects = (props: SanityProps<any>) => {
 
       <div className="z-20 relative bg-black" style={{ marginTop: heroSectionHeight }}>
         {renderObjectArray(page.sections, {
+          'ourProjectsPage.projects': Projects,
           newsletter: Newsletter,
           contact: Contact,
         })}
