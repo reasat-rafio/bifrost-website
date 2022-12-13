9bbd1cb845ed83e1e451a47f8891dac7eb0431b9
reasat-rafio <rafioar96@gmail.com>
2022-12-14 03:14:31 +0600

    :dizzy: studio rewrite

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const STUDIO_REWRITE = {
+  source: '/studio/:path*',
+  destination:
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:3333/studio/:path*'
+      : '/studio/index.html',
+}
+
 const withPlugins = require('next-compose-plugins')
 const withTM = require('next-transpile-modules')(['three'])
 const nextConfig = {
@@ -10,6 +19,7 @@ const nextConfig = {
     NEXT_PUBLIC_FORM_ID: process.env.NEXT_PUBLIC_FORM_ID,
   },
   reactStrictMode: true,
+  rewrites: async () => [STUDIO_REWRITE],
 }
 
 module.exports = withPlugins([withTM], nextConfig)
