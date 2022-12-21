e8022b3a818cc62e9ee6fa3fa5bd055bbc185beb
reasat-rafio <rafioar96@gmail.com>
2022-12-21 20:53:34 +0600

    :hammer: logistics page added

---
+++ apps/frontend/src/pages/logistics.tsx
@@ -0,0 +1,58 @@
+import { Contact } from 'components/common/contact'
+import { Hero, HeroProps } from 'components/common/primary-hero'
+import Newsletter from 'components/newsletter'
+import { siteQuery } from 'lib/query'
+import { GetStaticProps, GetStaticPropsContext } from 'next'
+import { groq } from 'next-sanity'
+import { SanityProps } from 'next-sanity-extra'
+import { useCallback, useState } from 'react'
+import { renderObjectArray, withDimensions } from 'sanity-react-extra'
+import { useSanityQuery, sanityStaticProps } from 'utils/sanity'
+
+const query = groq`{
+  "site": ${siteQuery},
+  "page": *[_id == "logisticsPage"][0] {
+    ...,
+    sections[] {
+      ...,
+      "image": ${withDimensions('image')},
+      projects[]{
+        ...,
+      "image": ${withDimensions('image')},
+      }
+    },
+  }
+}`
+
+export const getStaticProps: GetStaticProps = async (context: GetStaticPropsContext) => ({
+  props: await sanityStaticProps({ context, query }),
+  revalidate: 10,
+})
+
+const Logistics = (props: SanityProps<any>) => {
+  const {
+    data: { page },
+  } = useSanityQuery(query, props)
+  const [heroSectionHeight, setHeroSectionHeight] = useState(0)
+
+  return (
+    <>
+      {renderObjectArray(page.sections, {
+        primaryHero: useCallback(
+          (props: HeroProps) => <Hero {...props} setHeroSectionHeight={setHeroSectionHeight} />,
+          [],
+        ),
+      })}
+
+      <div className="z-20 relative bg-black" style={{ marginTop: heroSectionHeight }}>
+        {renderObjectArray(page.sections, {
+          //   'projects': Projects,
+          newsletter: Newsletter,
+          contact: Contact,
+        })}
+      </div>
+    </>
+  )
+}
+
+export default Logistics
