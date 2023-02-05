2f068d63363598acb215c46ff3b0e08a77e0ee73
reasat-rafio <rafioar96@gmail.com>
2023-02-05 16:55:45 +0600

    :passport_control: form schema validation added

--- apps/frontend/src/components/[recources]/form.tsx
+++ apps/frontend/src/components/[recources]/form.tsx
@@ -1,8 +1,34 @@
+import { useFormspark } from "@formspark/use-formspark";
+import { yupResolver } from "@hookform/resolvers/yup";
 import { Section } from "components/ui/section";
+import { FormSchema } from "lib/form-schema";
+import { useForm } from "react-hook-form";
 
 interface FormProps {}
 
-export const Form: React.FC<FormProps> = ({}) => {
+export const Form: React.FC<{}> = ({}) => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<FormProps>({
+    mode: "onChange",
+    resolver: yupResolver(FormSchema),
+  });
+  const [submit, submitting] = useFormspark({
+    formId: process.env.NEXT_PUBLIC_FORM_ID,
+  });
+
+  async function onSubmit({}: FormProps) {
+    try {
+      await submit({});
+      reset();
+    } catch (e) {
+    } finally {
+    }
+  }
+
   return (
     <Section
       padding={false}
--- apps/frontend/src/lib/form-schema.ts
+++ apps/frontend/src/lib/form-schema.ts
@@ -1,7 +1,25 @@
-import * as yup from 'yup'
+import * as yup from "yup";
 
 export const FormSchema = yup.object().shape({
   name: yup.string().max(20, "name can't be longer than 20 chars").required(),
-  email: yup.string().email('Please enter a valid email').required(),
+  email: yup.string().email("Please enter a valid email").required(),
   message: yup.string().required(),
-})
+});
+
+export const ResourcesFormSchema = yup.object().shape({
+  first_name: yup
+    .string()
+    .max(20, "name can't be longer than 20 chars")
+    .required(),
+  last_name: yup
+    .string()
+    .max(20, "name can't be longer than 20 chars")
+    .required(),
+  company_name: yup.string().max(50).required(),
+  work_email: yup.string().email("Please enter a correct email").required(),
+  job_title: yup.string().max(50).required(),
+  job_description: yup
+    .string()
+    .max(300, "Description can have a maximum of 300 words")
+    .required(),
+});
