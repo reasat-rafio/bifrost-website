04fdf77ce8d973d53b69364a1ade06fc27041bf8
reasat-rafio <rafioar96@gmail.com>
2023-02-07 15:28:15 +0600

    :adhesive_bandage: build errors fix

--- apps/frontend/src/pages/our-projects.tsx
+++ apps/frontend/src/pages/our-projects.tsx
@@ -1,14 +1,14 @@
-import { Contact } from 'components/common/contact'
-import { Newsletter } from 'components/common/newsletter'
-import { ProjectsBlock } from 'components/common/projects-block'
-import { Hero, HeroProps } from 'components/our-projects/hero'
-import { siteQuery } from 'lib/query'
-import { GetStaticProps, GetStaticPropsContext } from 'next'
-import { groq } from 'next-sanity'
-import { SanityProps } from 'next-sanity-extra'
-import { useCallback, useState } from 'react'
-import { renderObjectArray, withDimensions } from 'sanity-react-extra'
-import { useSanityQuery, sanityStaticProps } from 'utils/sanity'
+// import { Contact } from 'components/common/contact'
+import { Newsletter } from "components/common/newsletter";
+import { ProjectsBlock } from "components/common/projects-block";
+import { Hero, HeroProps } from "components/our-projects/hero";
+import { siteQuery } from "lib/query";
+import { GetStaticProps, GetStaticPropsContext } from "next";
+import { groq } from "next-sanity";
+import { SanityProps } from "next-sanity-extra";
+import { useCallback, useState } from "react";
+import { renderObjectArray, withDimensions } from "sanity-react-extra";
+import { useSanityQuery, sanityStaticProps } from "utils/sanity";
 
 const query = groq`{
   "site": ${siteQuery},
@@ -16,44 +16,51 @@ const query = groq`{
     ...,
     sections[] {
       ...,
-      "image": ${withDimensions('image')},
+      "image": ${withDimensions("image")},
       projects[]{
         ...,
-      "image": ${withDimensions('image')},
+      "image": ${withDimensions("image")},
       }
     },
   }
-}`
+}`;
 
-export const getStaticProps: GetStaticProps = async (context: GetStaticPropsContext) => ({
+export const getStaticProps: GetStaticProps = async (
+  context: GetStaticPropsContext
+) => ({
   props: await sanityStaticProps({ context, query }),
   revalidate: 10,
-})
+});
 
 const OurProjects = (props: SanityProps<any>) => {
   const {
     data: { page },
-  } = useSanityQuery(query, props)
-  const [heroSectionHeight, setHeroSectionHeight] = useState(0)
+  } = useSanityQuery(query, props);
+  const [heroSectionHeight, setHeroSectionHeight] = useState(0);
 
   return (
     <>
       {renderObjectArray(page.sections, {
-        'ourProjectsPage.hero': useCallback(
-          (props: HeroProps) => <Hero {...props} setHeroSectionHeight={setHeroSectionHeight} />,
-          [],
+        "ourProjectsPage.hero": useCallback(
+          (props: HeroProps) => (
+            <Hero {...props} setHeroSectionHeight={setHeroSectionHeight} />
+          ),
+          []
         ),
       })}
 
-      <div className="z-20 relative bg-black" style={{ marginTop: heroSectionHeight }}>
+      <div
+        className="relative z-20 bg-black"
+        style={{ marginTop: heroSectionHeight }}
+      >
         {renderObjectArray(page.sections, {
           projects: ProjectsBlock,
           newsletter: Newsletter,
-          contact: Contact,
+          // contact: Contact,
         })}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default OurProjects
+export default OurProjects;
--- apps/frontend/src/pages/why-synthetic-data.tsx
+++ apps/frontend/src/pages/why-synthetic-data.tsx
@@ -1,15 +1,15 @@
-import { Contact } from 'components/common/contact'
-import { Information } from 'components/common/information'
-import { Hero, HeroProps } from 'components/why-synthetic-data/hero'
-import { CollectionBlock } from 'components/common/header-and-card-collection-block'
-import { Resume } from 'components/why-synthetic-data/resume'
-import { siteQuery } from 'lib/query'
-import { GetStaticProps, GetStaticPropsContext } from 'next'
-import { groq } from 'next-sanity'
-import { SanityProps } from 'next-sanity-extra'
-import { useCallback, useState } from 'react'
-import { renderObjectArray, withDimensions } from 'sanity-react-extra'
-import { sanityStaticProps, useSanityQuery } from 'utils/sanity'
+// import { Contact } from 'components/common/contact'
+import { Information } from "components/common/information";
+import { Hero, HeroProps } from "components/why-synthetic-data/hero";
+import { CollectionBlock } from "components/common/header-and-card-collection-block";
+import { Resume } from "components/why-synthetic-data/resume";
+import { siteQuery } from "lib/query";
+import { GetStaticProps, GetStaticPropsContext } from "next";
+import { groq } from "next-sanity";
+import { SanityProps } from "next-sanity-extra";
+import { useCallback, useState } from "react";
+import { renderObjectArray, withDimensions } from "sanity-react-extra";
+import { sanityStaticProps, useSanityQuery } from "utils/sanity";
 
 const query = groq`{
   "site": ${siteQuery},
@@ -17,45 +17,52 @@ const query = groq`{
     ...,
     sections[] {
       ...,
-      "image": ${withDimensions('image')},
+      "image": ${withDimensions("image")},
       collection[]{
         ...,
-        "image": ${withDimensions('image')},
+        "image": ${withDimensions("image")},
       }
     },
   }
-}`
+}`;
 
-export const getStaticProps: GetStaticProps = async (context: GetStaticPropsContext) => ({
+export const getStaticProps: GetStaticProps = async (
+  context: GetStaticPropsContext
+) => ({
   props: await sanityStaticProps({ context, query }),
   revalidate: 10,
-})
+});
 
 const WhySyntheticData: React.FC<SanityProps> = (props) => {
   const {
     data: { page },
-  } = useSanityQuery(query, props)
-  const [heroSectionHeight, setHeroSectionHeight] = useState(0)
+  } = useSanityQuery(query, props);
+  const [heroSectionHeight, setHeroSectionHeight] = useState(0);
 
   return (
     <div className="relative">
       {renderObjectArray(page.sections, {
-        'whySyntheticDataPage.hero': useCallback(
-          (props: HeroProps) => <Hero {...props} setHeroSectionHeight={setHeroSectionHeight} />,
-          [],
+        "whySyntheticDataPage.hero": useCallback(
+          (props: HeroProps) => (
+            <Hero {...props} setHeroSectionHeight={setHeroSectionHeight} />
+          ),
+          []
         ),
       })}
 
-      <div className="relative z-10 bg-black" style={{ marginTop: heroSectionHeight }}>
+      <div
+        className="relative z-10 bg-black"
+        style={{ marginTop: heroSectionHeight }}
+      >
         {renderObjectArray(page.sections, {
           infoBlock: Information,
           headerAndcollectionOfItems: CollectionBlock,
           resume: Resume,
-          contact: Contact,
+          // contact: Contact,
         })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default WhySyntheticData
+export default WhySyntheticData;
