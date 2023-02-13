ceeed0443c5748d8531cd7392efc4a53bc526783
reasat-rafio <rafioar96@gmail.com>
2023-02-13 10:48:28 +0600

    :hammer: spacing adjustment for Boxed CTA section

--- apps/frontend/src/components/common/information.tsx
+++ apps/frontend/src/components/common/information.tsx
@@ -40,11 +40,12 @@ export const Information: React.FC<InformationProps> = ({
   );
 
   return (
-    <GradientBorder innerClass="md:py-16 py-8" ref={sectionRef}>
+    <GradientBorder innerClass="md:p-16 sm:p-8 p-5" ref={sectionRef}>
       <Section
+        isContainer={false}
         borderBottom={false}
         padding={false}
-        className="flex flex-col-reverse md:flex-row"
+        className="flex flex-col-reverse pb-4 sm:pb-0 md:flex-row"
       >
         <section className="mt-10 flex w-full flex-1 justify-center md:mt-0">
           <div className="spacing_primary my-auto mr-auto h-fit w-full md:w-[85%]">
@@ -84,7 +85,7 @@ export const Information: React.FC<InformationProps> = ({
           className="w-full flex-1 overflow-hidden rounded-primary"
         >
           <SanityImg
-            className="h-full w-full object-contain"
+            className="h-full max-h-[515px] w-full object-cover"
             image={image}
             alt={image.alt}
             builder={imageUrlBuilder}
--- apps/frontend/src/components/ui/description.tsx
+++ apps/frontend/src/components/ui/description.tsx
@@ -25,8 +25,8 @@ export const Description: React.FC<DescriptionProps> = ({
       variant === "big"
         ? "lg:text-p-1 sm:text-3xl text-2xl md:!leading-[48px] md:!tracking-[2px] tracking-[0.02em] leading-[30px]"
         : variant === "small"
-        ? "lg:text-p-3 sm:text-xl text-p-3-mobile !tracking-[0.02em] "
-        : "lg:text-p-2 sm:text-2xl text-xl !tracking-[0.02em] "
+        ? "lg:text-p-3 sm:text-xl text-p-3-mobile !tracking-[0.02em] md:!leading-[30px] leading-[25px]"
+        : "lg:text-p-2 sm:text-2xl text-xl !tracking-[0.02em] md:!leading-[30px] leading-[25px]"
     }`,
   };
 
