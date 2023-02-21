82262d31c3984d8ff5bb3a6644d32a116b9c1b1b
reasat-rafio <rafioar96@gmail.com>
2023-02-21 18:10:16 +0600

    :recycle: about us section font and image res adjusmetn

--- apps/frontend/src/components/landing/about-us.tsx
+++ apps/frontend/src/components/landing/about-us.tsx
@@ -91,7 +91,9 @@ const DescriptionBlock: React.FC<IDescriptionBlock> = ({
   return (
     <section className="spacing_primary | font-light">
       {!!title && <Title el="h3">{title}</Title>}
-      <Heading el="h2">{heading}</Heading>
+      <Heading variant="small" el="h2">
+        {heading}
+      </Heading>
       <Description
         type="div"
         variant="small"
@@ -102,7 +104,7 @@ const DescriptionBlock: React.FC<IDescriptionBlock> = ({
           serializers={{
             listItem: ({ children }: any) => {
               return (
-                <li>
+                <li className="">
                   <div className="mb-5 flex items-center space-x-3">
                     <span
                       style={{
@@ -110,7 +112,7 @@ const DescriptionBlock: React.FC<IDescriptionBlock> = ({
                       }}
                       className="h-3 w-3 rounded-[4px] bg-[#B794FF] sm:h-[15.18px] sm:w-[15.18px]"
                     />
-                    <span>{children}</span>
+                    <span className="flex-1">{children}</span>
                   </div>
                 </li>
               );
@@ -132,7 +134,7 @@ const ImageBlock: React.FC<IImageBlock> = ({ image, windowWidth }) => {
     <motion.figure>
       <SanityImg
         className="h-full w-full rounded-primary object-cover"
-        width={windowWidth >= 1280 ? 800 : windowWidth >= 640 ? 400 : 300}
+        width={windowWidth >= 1280 ? 900 : windowWidth >= 640 ? 500 : 300}
         image={image}
         builder={imageUrlBuilder}
         alt={image.alt}
